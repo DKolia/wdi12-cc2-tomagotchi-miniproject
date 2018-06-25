a251862a3c70c45edce28dd0e1dc666c2b53638d
David Kolia <Dkolia@icloud.com>
2018-06-25 18:52:45 -0500

    Built HTML and CSS, began pseudocode for JS

---
+++ js/app.js
@@ -0,0 +1,39 @@
+console.log("Hello there!");
+//
+//  Set up Needs.
+//  3 Needs total.
+//  Food, Sleep, Fun.
+//  Food 0-10.
+//  Sleep 0-10.
+//  Fun 0-10.
+//  If any need falls to zero, notagotchi dies
+//  notagotchi lose 1 point every give minutes
+//  
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
