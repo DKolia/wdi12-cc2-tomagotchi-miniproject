164d3a12e02393060132ca51e6cbbfb7f541286e
David Kolia <Dkolia@icloud.com>
2018-06-25 19:32:02 -0500

    Added pseudocode for game logic

--- js/app.js
+++ js/app.js
@@ -1,39 +1,52 @@
 console.log("Hello there!");
-//
-//  Set up Needs.
-//  3 Needs total.
-//  Food, Sleep, Fun.
-//  Food 0-10.
-//  Sleep 0-10.
-//  Fun 0-10.
-//  If any need falls to zero, notagotchi dies
-//  notagotchi lose 1 point every give minutes
-//  
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
+
+//  Life Logic:
+//  Notagotchi begins in egg
+//  Notagotchi "age" one "level" every 7 minutes
+//  Each "age" has a new image to update
+//  Will eventually die of old age
+//  When notagotchi dies, display RIP image for 5 seconds
+
+
+//  Death Logic:
+//  Notagotchi dies when any Need falls to zero
+//  Notagotchi dies when maximumLifeSpan is met- no matter what
+//  Notagotchi death displays the RIP image for 5 seconds, then displays egg
+
+
+//  Egg Logic:
+//  Notagotchi starts in egg form
+//  Notagotchi stays in egg form forever until...
+//  Notagotchi "hatches" to first form when any Need button is clicked
+//  Notagotchi egg is brought back 5 seconds after Notagotchi death
+//  Cycle repeats
+
+
+//  Variables to name:
+//  Food (0-10)
+//  Fun (0-10)
+//  Sleep (0-10)
+//  Age (0-10
+//  maximumLifeSpan (10% chance @ age 5, 30% change @ age 6, 50% chance at age 7, 70% chance at age 8, 90% chance at age 9, 100% chance dead by age 10
+//  gameClock (real life full seconds)
+
+
+//  Food Logic:
+//  Notagotchi starts with 3 Food
+//  Food level counts one-by-one down to zero every five minutes
+//  Clicking "feed" button raises food to 10 instantly
+//  Clicking "feed" button displays food image for 3 seconds
+
+
+//  Fun Logic:
+//  Notagotchi starts with 5 Fun
+//  Fun level counts one-by-one down to zero every five minutes
+//  Clicking "play" button raises fun to 10 instantly
+//  Clicking "play" button displays play image for 3 seconds
+
+
+//  Sleep Logic:
+//  Notagotchi starts with 3 Sleep
+//  Sleep level counts one-by-one down to zero every five minutes
+//  Clicking "rest" button raises sleep to 10 instantly
+//  Clicking "rest" button displays sleep image for 3 seconds
